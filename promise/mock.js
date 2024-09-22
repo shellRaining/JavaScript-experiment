eba2bae31031efaa74cd5abffdc2afefd2cab6b1
shellRaining <shellRaining@gmail.com>
2024-09-22 21:17:52 +0800

    Update mock.js

--- promise/mock.js
+++ promise/mock.js
@@ -1,149 +1,104 @@
-const PENDING = "pending";
-const FULFILLED = "fulfilled";
-const REJECTED = "rejected";
+const PENDING = "PENDING";
+const FULFILLED = "FULFILLED";
+const REJECTED = "REJECTED";
 
-function Promise(fn) {
-  this.status = PENDING;
-  this.value = null;
-  this.reason = null;
-  this.onFulfilledCallbacks = [];
-  this.onRejectedCallbacks = [];
+class Promise {
+  constructor(fn) {
+    this.status = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+    this.onFulfilledCbs = [];
+    this.onRejectedCbs = [];
 
-  const resolve = (value) => {
-    if (this.status === PENDING) {
-      this.status = FULFILLED;
+    const resolve = (value) => {
+      if (this.status !== PENDING) return;
       this.value = value;
-      this.onFulfilledCallbacks.forEach((cb) => cb());
-    }
-  };
-  const reject = (reason) => {
-    if (this.status === PENDING) {
-      this.status = REJECTED;
+      this.status = FULFILLED;
+      this.onFulfilledCbs.forEach((cb) => void cb());
+    };
+    const reject = (reason) => {
+      if (this.status !== PENDING) return;
       this.reason = reason;
-      this.onRejectedCallbacks.forEach((cb) => cb());
-    }
-  };
+      this.status = REJECTED;
+      this.onRejectedCbs.forEach((cb) => void cb());
+    };
 
-  try {
-    fn(resolve, reject);
-  } catch (e) {
-    reject(e);
-  }
-}
-
-function resolvePromise(promise, x, resolve, reject) {
-  if (promise === x) return reject(new TypeError("promise equal with res"));
-  if (x === null) return resolve(null);
-  else if (typeof x !== "function" && typeof x !== "object") return resolve(x);
-  else if (x instanceof Promise) {
-    return x.then(
-      (value) => resolvePromise(promise, value, resolve, reject),
-      reject,
-    );
-  }
-
-  let then;
-  try {
-    then = x.then;
-    if (typeof then !== "function") return resolve(x);
-  } catch (e) {
-    reject(e);
-  }
-  var called = false;
-  // 将 x 作为函数的作用域 this 调用之
-  // 传递两个回调函数作为参数，第一个参数叫做 resolvePromise ，第二个参数叫做 rejectPromise
-  // 名字重名了，我直接用匿名函数了
-  try {
-    then.call(
-      x,
-      // 如果 resolvePromise 以值 y 为参数被调用，则运行 [[Resolve]](promise, y)
-      function (y) {
-        // 如果 resolvePromise 和 rejectPromise 均被调用，
-        // 或者被同一参数调用了多次，则优先采用首次调用并忽略剩下的调用
-        // 实现这条需要前面加一个变量called
-        if (called) return;
-        called = true;
-        resolvePromise(promise, y, resolve, reject);
-      },
-      // 如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
-      function (r) {
-        if (called) return;
-        called = true;
-        reject(r);
-      },
-    );
-  } catch (error) {
-    // 如果调用 then 方法抛出了异常 e：
-    // 如果 resolvePromise 或 rejectPromise 已经被调用，则忽略之
-    if (called) return;
-
-    // 否则以 e 为据因拒绝 promise
-    reject(error);
-  }
-}
-
-Promise.prototype.then = function (onFulfilled, onRejected) {
-  const fulfilledTask = (resolve, reject) => {
     try {
-      if (typeof onFulfilled === "function") {
-        const res = onFulfilled(this.value);
-        resolvePromise(resPromise, res, resolve, reject);
-      } else {
-        resolve(this.value);
-      }
+      fn(resolve, reject);
     } catch (e) {
       reject(e);
     }
-  };
-  const rejectedTask = (resolve, reject) => {
-    try {
-      if (typeof onRejected === "function") {
-        const res = onRejected(this.reason);
-        resolvePromise(resPromise, res, resolve, reject);
-      } else {
-        reject(this.reason);
-      }
-    } catch (e) {
-      reject(e);
-    }
-  };
-
-  let resPromise;
-  if (this.status === FULFILLED) {
-    resPromise = new Promise((resolve, reject) => {
-      setTimeout(fulfilledTask, 0, resolve, reject);
-    });
-  } else if (this.status === REJECTED) {
-    resPromise = new Promise((resolve, reject) => {
-      setTimeout(rejectedTask, 0, resolve, reject);
-    });
-  } else {
-    resPromise = new Promise((resolve, reject) => {
-      this.onFulfilledCallbacks.push(() => {
-        setTimeout(fulfilledTask, 0, resolve, reject);
-      });
-      this.onRejectedCallbacks.push(() => {
-        setTimeout(rejectedTask, 0, resolve, reject);
-      });
-    });
   }
-  return resPromise;
-};
 
-Promise.deferred = function () {
-  var result = {};
-  result.promise = new Promise(function (resolve, reject) {
-    result.resolve = resolve;
-    result.reject = reject;
-  });
+  then(onFulfilled, onRejected) {
+    const resPromise = new Promise((resolve, reject) => {
+      const resolveThen = (cbRet) => {
+        if (cbRet === resPromise) return reject(new TypeError());
 
-  return result;
-};
+        if (cbRet instanceof Promise) cbRet.then(resolveThen, reject);
+        else if (
+          typeof cbRet === "function" ||
+          (typeof cbRet === "object" && cbRet !== null)
+        ) {
+          let then;
+          try {
+            then = cbRet.then;
+            if (typeof then !== "function") resolve(cbRet);
+          } catch (e) {
+            reject(e);
+          }
 
-Promise.reject = function (reason) {
-  return new Promise((resolve, reject) => {
-    reject(reason);
-  });
-};
+          let called = false;
+          function callOnce(fn) {
+            return function (...args) {
+              if (called) return;
+              called = true;
+              fn(...args);
+            };
+          }
+          try {
+            then.call(cbRet, callOnce(resolveThen), callOnce(reject));
+          } catch (e) {
+            callOnce(reject)(e);
+          }
+        } else resolve(cbRet);
+      };
+      const fulfilledTask = () => {
+        try {
+          if (typeof onFulfilled !== "function") resolve(this.value);
+          else resolveThen(onFulfilled(this.value));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      const rejectedTask = () => {
+        try {
+          if (typeof onRejected !== "function") reject(this.reason);
+          else resolveThen(onRejected(this.reason));
+        } catch (e) {
+          reject(e);
+        }
+      };
+      if (this.status === FULFILLED) queueMicrotask(fulfilledTask);
+      else if (this.status === REJECTED) queueMicrotask(rejectedTask);
+      else {
+        this.onFulfilledCbs.push(() => queueMicrotask(fulfilledTask));
+        this.onRejectedCbs.push(() => queueMicrotask(rejectedTask));
+      }
+    });
+
+    return resPromise;
+  }
+
+  static deferred() {
+    var result = {};
+    result.promise = new Promise(function (resolve, reject) {
+      result.resolve = resolve;
+      result.reject = reject;
+    });
+
+    return result;
+  }
+}
 
 module.exports = Promise;
